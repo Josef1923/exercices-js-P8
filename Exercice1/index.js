dade1e5e60c2154d6c14bdc206bb87d0c7bd6522
Josef1923 <schererjos01@gmail.com>
2024-10-07 16:23:12 +0200

    Exercices 1 & 2

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,11 @@
 //votre code ici
+function pairNumbers(a, b) {
+    let result = [];
+    for (let i = a; i <= b; i++) {
+        if (i % 2 === 0)
+            result.push(i)
+    }
+    return result.join(',')
+}
 
 export default pairNumbers
--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,5 +1,34 @@
 // Créez votre fonction ici
+function calculate(a, b, operateur) {
+    let result;
 
+    a = Number(a);
+    b = Number(b);
+
+    if (operateur === '+') {
+        result = a + b;
+    }
+    if (operateur === '-') {
+        result = a - b;
+    }
+    if (operateur === '*') {
+        result = a * b;
+    }
+    if (operateur === '/') {
+        if (b === 0) {
+            return 'Division by zero is not allowed';
+        } else {
+            result = a / b;
+        }
+    }
+
+    const operateurValides = ['+', '-', '*', '/'];
+    if (!operateurValides.includes(operateur)) {
+        return 'Invalid operator';
+    }
+
+    return result;
+}
 // Exemples d'utilisation de la fonction
 console.log(calculate(5, 3, '+'));   // Affiche 8
 console.log(calculate(10, 4, '-'));  // Affiche 6
